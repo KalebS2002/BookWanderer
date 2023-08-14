bd71b70a3066ed78f084680956f9e225c4c89460
Kaleb <KalebStephens122@gmail.com>
2023-08-14 15:40:13 -0500

    added nav comp

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { App } from './components';
+import React from "react";
+import ReactDOM from "react-dom";
+import { App } from "./components";
 // css stylesheets can be created for each component
 // place them in the src/style directory, and import them like this:
-import './style/index.css';
+import "./style/index.css";
+import { BrowserRouter } from "react-router-dom/cjs/react-router-dom.min";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
+ShadowRoot.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
