32dcf9c08662e1b5042fa7d3167203c3ea0f8702
savstew <stewartsavhanna@gmail.com>
2023-08-15 22:28:13 -0400

    added register route

--- api/users.js
+++ api/users.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const usersRouter = express.Router();
 
-const { getAllUsers } = require("../db");
+const { getAllUsers, getUserByUsername, createUser } = require("../db");
 
 usersRouter.use((req, res, next) => {
   console.log("A request is being made to /api/users - next() is called ...");
@@ -19,5 +19,41 @@ usersRouter.get("/", async (req, res, next) => {
     next(error);
   }
 });
+
+// POST /api/users/register
+router.post('/register', async (req, res, next) => {
+    const { username, password, useremail } = req.body;
+  
+    try {
+      const isadmin = false;
+      const _user = await getUserByUsername(username)
+
+      if (_user) {
+        res.send({
+            message: `User ${username} is already taken.`,
+            name: 'UserTakenError',
+            error: 'UserTakenError'
+        });
+        throw new Error('UserTakenError')
+      }
+
+      const user = await createUser({
+        username,
+        useremail,
+        password,
+        isadmin
+      });
+  
+      res.send({ 
+        message: "thank you for signing up",
+        user: {
+            username: user.username,
+            useremail: user.useremail
+        } 
+      });
+    }  catch ({ name, message}) {
+        next({ name, message})
+      } 
+  });
 
 module.exports = usersRouter;
