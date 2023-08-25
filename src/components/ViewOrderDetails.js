6e7e563b481be554e6e3b004b515fa0deb98a821
KarlaBott <quadkhouse@hotmail.com>
2023-08-25 16:25:45 -0500

    updated purchased order styling

--- src/components/ViewOrderDetails.js
+++ src/components/ViewOrderDetails.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "../style/Products.css";
 import "../style/ViewOrderDetails.css";
 
 const ViewOrderDetails = ({ purchasedOrder }) => {
@@ -7,17 +8,43 @@ const ViewOrderDetails = ({ purchasedOrder }) => {
 
   return (
     <>
-      <button>
-        <Link to="/orderhistory">Return to OrderHistory</Link>
-      </button>
-      {purchasedOrder.orderdetails.map((orderDetail) => (
-        <div id="orderDetailBox" key={orderDetail.productid}>
-          <div>Title: {orderDetail.title}</div>
-          <div>Author: {orderDetail.author}</div>
-          <div>ItemPrice: ${orderDetail.itemprice}</div>
-          <div>Quantity: {orderDetail.quantity}</div>
+      <section id="orderHeader">
+        <button>
+          <Link to="/orderhistory">Return to OrderHistory</Link>
+        </button>
+
+        <div>Order Detail for Order ID: {purchasedOrder.id}</div>
+        <div>Purchased on: {purchasedOrder.lastupdate.substring(0, 10)}</div>
+        <div>Total for this order was: ${purchasedOrder.ordertotal}</div>
+        <div>
+          {" "}
+          - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -{" "}
         </div>
-      ))}
+        <div>Items included in this order . . .</div>
+      </section>
+      <div id="productsBody">
+        {purchasedOrder.orderdetails.map((orderDetail) => (
+          <div className="row" key={orderDetail.productid}>
+            <div id="productsContainer">
+              <div id="imgSection">
+                <img
+                  id="productImg"
+                  src={orderDetail.imageurl}
+                  alt={orderDetail.title}
+                />
+              </div>
+              <div className="productCard">Title: {orderDetail.title}</div>
+              <div className="productCard">Author: {orderDetail.author}</div>
+              <div className="productCard">
+                Item Price: ${orderDetail.itemprice}
+              </div>
+              <div className="productCard">
+                Quantity: {orderDetail.quantity}
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
     </>
   );
 };
