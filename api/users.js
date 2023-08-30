26d82feeded1ca76666ab406dea089d804bdedd9
savstew <stewartsavhanna@gmail.com>
2023-08-30 10:11:39 -0400

    small changes

--- api/users.js
+++ api/users.js
@@ -35,12 +35,16 @@ usersRouter.get("/", async (req, res, next) => {
 
 // POST /api/users/register
 usersRouter.post("/register", async (req, res, next) => {
+  //Destructures the request body and parses out username, password, and useremail
   const { username, password, useremail } = req.body;
 
+  //Attempt to create the user in the database
   try {
     const isadmin = false;
+    //check to see if username already exists in database
     const _user = await getUserByUsername(username);
 
+    //If there is an existing user with the username, return an error
     if (_user) {
       return next({
         message: `User ${username} is already taken.`,
@@ -49,6 +53,7 @@ usersRouter.post("/register", async (req, res, next) => {
       });
     }
 
+    //Create the new user
     const user = await createUser({
       username,
       useremail,
@@ -56,8 +61,10 @@ usersRouter.post("/register", async (req, res, next) => {
       isadmin,
     });
 
+    //Updates current guest order for new user
     await updateCurrentGuestOrderForNewUser(user.id);
 
+    //Send back successful response with user and message
     res.send({
       message: "REGISTRATION SUCCESSFUL!  Thank you for signing up",
       user: {
@@ -74,19 +81,23 @@ usersRouter.post("/register", async (req, res, next) => {
 // POST /api/users/login
 usersRouter.post("/login", async (req, res, next) => {
   try {
+    //Gets user if exists in database by the request body's username
     const user = await getUserByUsername(req.body.username);
 
+    //If the user does not exist, send back an error
     if (!user) {
       return next({
         message: "That user does not exist, please try another username.",
       });
     }
 
+    //Compare the request body's username and the encrypted database password for the respective user
     const isPasswordValid = await bcrypt.compare(
       req.body.password,
       user.password
     );
 
+    //If passwords do not match, send back an error
     if (!isPasswordValid) {
       return next({
         message:
@@ -94,11 +105,15 @@ usersRouter.post("/login", async (req, res, next) => {
       });
     }
 
+    //Remove unnessecary information from user object
     delete user.password;
     delete user.isadmin;
 
+  //Updates current guest order for new user
     await updateCurrentGuestOrderForExistingUser(user.id);
     console.log(user);
+    
+    //Send back successful response with user and message
     res.send({ user, message: "SUCCESSFULLY logged in!" });
   } catch (error) {
     next(error);
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,30 +1,43 @@
 import React, { useState, useEffect } from "react";
-import { Link, Redirect } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 import TreeIcon from "../Images/TreeIcon.png";
 import { login } from "../axios-services/users";
 import "../style/Login_Signup.css";
 
+//Component login which takes in 2 props (isLoggedIn => bool, setIsLoggedIn => function)
 function Login({ isLoggedIn, setIsLoggedIn }) {
+  //Sets the states which we use later in the component
   const [Password, setPassword] = useState("");
   const [Username, setUsername] = useState("");
   const [message, setMessage] = useState("");
+  //Used to route to other pages; In this we route to products after a successful log in
+  let history = useHistory()
 
+  //if isLoggedIn changes refresh page
   useEffect(() => {}, [isLoggedIn]);
 
+  //Function used to handle form submission
   async function HandleForm(event) {
     event.preventDefault();
+    //Function which takes in Username and Password. If successful will return a copy of the user | else return error with message
     let response = await login(Username, Password);
     let curUserid = sessionStorage.getItem("BWUSERID");
     console.log("Login > BWUSERID:", curUserid);
+    //checks if user is logged in by seeing if parsed integer is greater than 1
     if (parseInt(curUserid) > 1) {
       setIsLoggedIn(true);
     } else {
       setIsLoggedIn(false);
     }
+    //Sets the message from the response after the sign in
     setMessage(response.message);
-    // <Redirect to="/products" />;
+
+    //If sign up is successful then reroute to products page
+    if(response.user)
+      history.push("/products")
   }
 
+  //return user ui with textboxes which set our above states
   return (
     <form id="Login" onSubmit={HandleForm}>
       <label className="LoginLabel">Login</label>
